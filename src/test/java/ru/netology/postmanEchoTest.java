3ef08c25d083b83caad5d6038b9f9c38502595cf
Shabalin Sergey <sergeyshm38@gmail.com>
2022-07-24 17:40:51 +0300

    Step for Fail run

--- src/test/java/ru/netology/postmanEchoTest.java
+++ src/test/java/ru/netology/postmanEchoTest.java
@@ -20,7 +20,7 @@ public class postmanEchoTest {
 // Проверки
                 .then()
                     .statusCode(200)
-                    .body("data", equalTo("ru.netology.api.postman"))
+                    .body("data", equalTo("raw "))
         ;
     }
 }
